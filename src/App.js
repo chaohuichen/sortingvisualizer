5de9554dddb88bca3cc9eac4d901bcffa659a0a7
chaohuichen <chenpeter0821@gmail.com>
2020-11-05 19:08:34 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import SingleChart from './SingleChart'
+function App() {
+
+  return (
+    <div className="App">
+      <SingleChart/>
+    </div>
+  );
+}
+
+export default App;
